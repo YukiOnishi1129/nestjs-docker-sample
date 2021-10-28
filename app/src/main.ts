47529b7e0b3ad0c00af6617fd680a6b2497b3f1c
yuki <yuki.onishi1129@gmail.com>
2021-10-28 14:55:24 +0900

    add: swagger setting

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // APIのURLを全て「/api」から始まるようにする
+  app.setGlobalPrefix('api');
+
+  // CORS対応
+  app.enableCors();
+
+  // Swagger拡張の有効化
+  const config = new DocumentBuilder()
+    .setTitle('API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(4000);
 }
 bootstrap();
